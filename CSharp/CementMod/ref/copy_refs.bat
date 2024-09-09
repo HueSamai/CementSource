627524e00b23e7dd179f63cd028d95b829e3e36d
The_UltimateNuke <84410206+TheUltimateNuke@users.noreply.github.com>
2024-09-09 15:49:51 -0400

    fixed copy_refs.bat

--- CSharp/CementMod/ref/copy_refs.bat
+++ CSharp/CementMod/ref/copy_refs.bat
@@ -3,5 +3,5 @@ set game_dir
 set game_dir=%game_dir:"=%
 set game_dir
 
-xcopy "%game_dir%\MelonLoader\net6\" ".\net6" /s /e /y
-xcopy "%game_dir%\MelonLoader\Il2CppAssemblies\" ".\Il2CppAssemblies" /s /e /y
+xcopy "%game_dir%\MelonLoader\net6\" ".\ref\net6" /s /e /y
+xcopy "%game_dir%\MelonLoader\Il2CppAssemblies\" ".\ref\Il2CppAssemblies" /s /e /y
