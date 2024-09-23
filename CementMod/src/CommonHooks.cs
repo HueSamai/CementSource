2a9ebf0f7b25e069c8d3a18205d618308b005939
The_UltimateNuke <84410206+TheUltimateNuke@users.noreply.github.com>
2024-09-23 15:33:04 -0400

    added some more common hooks for game lifecycle

--- CementMod/src/CommonHooks.cs
+++ CementMod/src/CommonHooks.cs
@@ -1,10 +1,11 @@
-using System;
+using Il2CppCoreNet.Components;
 using MelonLoader;
+using System;
 
 namespace CementGB.Mod;
 
 /// <summary>
-/// Provides some useful hooks for certain in-game events.
+/// Provides some useful shorthand hooks for certain in-game events.
 /// </summary>
 public static class CommonHooks
 {
@@ -12,12 +13,25 @@ public static class CommonHooks
     /// Fired when the Menu scene loads for the first time in the app's lifespan. Will reset on application quit.
     /// </summary>
     public static event Action? OnMenuFirstBoot;
+    public static event Action? OnGameSetup;
+    public static event Action? OnGameStart;
+    public static event Action? OnGameEnd;
 
     private static bool _menuFirstBoot;
 
     internal static void Initialize()
     {
         MelonEvents.OnSceneWasLoaded.Subscribe(OnSceneWasLoaded);
+        OnMenuFirstBoot += Internal_MenuFirstBoot;
+    }
+
+    private static void Internal_MenuFirstBoot()
+    {
+        var netRoundOrganizer = NetRoundOrganiser.Instance;
+
+        netRoundOrganizer.OnGameSetup.CombineImpl((Il2CppSystem.Action)OnGameSetup);
+        netRoundOrganizer.OnGameStart.CombineImpl((Il2CppSystem.Action)OnGameStart);
+        netRoundOrganizer.OnGameEnded.CombineImpl((Il2CppSystem.Action)OnGameEnd);
     }
 
     private static void OnSceneWasLoaded(int buildIndex, string sceneName)
