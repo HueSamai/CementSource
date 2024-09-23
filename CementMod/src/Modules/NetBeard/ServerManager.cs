aab173325e941709fc26c05232c6353fe334528c
Ethan Barron <83491820+EthanJamesBarron@users.noreply.github.com>
2024-09-23 16:03:37 +0100

    sER

--- CementMod/src/Modules/NetBeard/ServerManager.cs
+++ CementMod/src/Modules/NetBeard/ServerManager.cs
@@ -1,18 +1,13 @@
 ﻿using Il2Cpp;
-using Il2CppBootstrap;
 using Il2CppCoatsink.UnityServices;
-using Il2CppCoreNet;
 using Il2CppCoreNet.Config;
 using Il2CppGB.Config;
-using Il2CppGB.Core;
-using Il2CppGB.Core.Bootstrappers;
-using Il2CppGB.Game;
+using Il2CppGB.UnityServices.Matchmaking;
 using System;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
-namespace CementGB.Mod.Modules.NetBeard;
+namespace CementGB.Mod.Utilities;
 
 [MelonLoader.RegisterTypeInIl2Cpp]
 internal class ServerManager : MonoBehaviour
@@ -22,7 +17,10 @@ internal class ServerManager : MonoBehaviour
 
     void Awake()
     {
-        if (Environment.GetCommandLineArgs().Contains("-SERVER")) isServer = true;
+        if (Environment.GetCommandLineArgs().Contains("-SERVER"))
+        {
+            isServer = true;
+        }
 
         SceneManager.add_sceneLoaded(new Action<Scene, LoadSceneMode>(WrapperFix));
     }
@@ -32,32 +30,30 @@ internal class ServerManager : MonoBehaviour
         if (menuHasLoadedPreviously) return;
         if (scene.name == "Menu")
         {
-/*            NetworkBootstrapper.IsDedicatedServer = true;
-            FindObjectOfType<NetworkBootstrapper>().AutoRunServer = true;*/
-
             menuHasLoadedPreviously = true;
+
             gameObject.AddComponent<DevelopmentTestServer>().ui = GameObject.Find("Global(Clone)/UI/PlatformUI/Development Server Menu").GetComponent<DevelopmentTestServerUI>();
 
             // Launching from my server terminal gives the server arg which just mutes the game, hides the load level UI (it bugs out) and attempts to
             // initialize integral coatsink wrappers
-            if (Environment.GetCommandLineArgs().Contains("-SERVER"))
-                if (isServer)
-                {
-                    UnityServicesManager.Instance.Initialise(UnityServicesManager.InitialiseFlags.DedicatedServer, null, "", "DGS");
-                    AudioListener.pause = true;
-                    GameObject.Find("Global(Clone)/LevelLoadSystem").SetActive(false);
-                }
+            if (isServer)
+            {
+                UnityServicesManager.Instance.Initialise(UnityServicesManager.InitialiseFlags.DedicatedServer, null, "", "DGS");
+                AudioListener.pause = true;
+                GameObject.Find("Global(Clone)/LevelLoadSystem").SetActive(false);
+            }
         }
     }
 }
 
-[HarmonyLib.HarmonyPatch(typeof(DevelopmentTestServer), nameof(DevelopmentTestServer.SetupLocalServer))]
+[HarmonyLib.HarmonyPatch(typeof(DevelopmentTestServer), nameof(DevelopmentTestServer.Awake))]
 public static class CLAFix
 {
-    public static void Postfix(DevelopmentTestServer __instance, RotationConfig gameConfig, ServerConfig serverConfig)
+    public static void Postfix()
     {
         string valueForKey = CommandLineParser.Instance.GetValueForKey("-DDC_IP", true);
         string valueForKey2 = CommandLineParser.Instance.GetValueForKey("-DDC_PORT", true);
+
         if (!string.IsNullOrEmpty(valueForKey2))
         {
             DevelopmentTestServer.DirectConnectPort = int.Parse(valueForKey2);
